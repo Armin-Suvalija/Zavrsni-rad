9c27e58a955244e0e8a3e28e7a38cef552a0e023
Armin-Suvalija <armin.suve@gmail.com>
2025-07-14 21:43:08 +0200

    Delete zavrsni_projekat directory

--- zavrsni_projekat/etl/extract/extractor.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_dataset(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-    print(f"Loaded {len(df)} rows and {len(df.columns)} columns.")
-    return df
--- zavrsni_projekat/etl/transform/transformer.py
+++
@@ -1,4 +0,0 @@
-def clean_dataset(df):
-    df = df.drop_duplicates()
-    df = df.fillna(0)
-    return df

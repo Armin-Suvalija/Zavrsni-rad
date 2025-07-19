cbe9e37d27e00ba8ed900d733636b7f32c6282cc
Armin-Suvalija <armin.suve@gmail.com>
2025-07-19 15:50:21 +0200

    Delete zavrsni_projekat directory

--- zavrsni_projekat/etl/load/loader.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-def load_to_mssql(df, table_name, connection_string):
-    engine = create_engine(connection_string)
-    df.to_sql(table_name, con=engine, if_exists='replace', index=False)
-    print(f"Data loaded to table: {table_name}")
